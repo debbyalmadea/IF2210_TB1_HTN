b4e718fc4d28bfff6d2b00f6cbf0f9d1be0930f4
Mifkiyan <m.favian199@gmail.com>
2023-03-12 19:36:27 +0700

    [ADD] store cards to player

--- src/Player/Player.hpp
+++ src/Player/Player.hpp
@@ -1,24 +1,30 @@
 #ifndef _PLAYER_
 #define _PLAYER_
 #include "../Ability/Ability.hpp"
-// PLAYER class
+#include "../Card/PermenCard.hpp"
+#include <utility>
+//  PLAYER class
 
 class Player
 {
 protected:
     int id;
     int point;
-    // todo : Ability Card
+    pair<PermenCard, PermenCard> cards;
     Ability ability;
     bool sudahGiliran;
 
 public:
     Player() : id(0), point(0), sudahGiliran(false)
     {
+        cards.first = PermenCard();
+        cards.second = PermenCard();
         ability = Ability();
     };
     Player(int _id) : id(_id), point(0), sudahGiliran(false)
     {
+        cards.first = PermenCard();
+        cards.second = PermenCard();
         ability = Ability();
     };
     int getID() const
@@ -28,6 +34,18 @@ public:
     int getPoint() const
     {
         return point;
+    }
+    PermenCard getFirstCard() const
+    {
+        return cards.first;
+    }
+    PermenCard getSecondCard() const
+    {
+        return cards.second;
+    }
+    pair<PermenCard, PermenCard> getBothCard() const
+    {
+        return cards;
     }
     Ability getAbility() const
     {
